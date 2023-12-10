13ce7ca2622f372ed0fc10ab28b4a0a1d9724234
parksomin72 <fatimazahraealawi@gmail.com>
2023-12-10 12:52:11 +0000

    .

--- console.py
+++ console.py
@@ -3,27 +3,27 @@
 import cmd
 
 class Console(cmd.Cmd):
-    """Console class for AirBnB command interpreter."""
+"""Console class for AirBnB command interpreter."""
 
-    prompt = "(hbnb) "
+prompt = "(hbnb) "
 
-    def do_help(self, arg):
-        """Display help information."""
-        cmd.Cmd.do_help(self, arg)
+def do_help(self, arg):
+"""Display help information."""
+cmd.Cmd.do_help(self, arg)
 
-    def do_quit(self, arg):
-        """Exit the command interpreter."""
-        return True
+def do_quit(self, arg):
+"""Exit the command interpreter."""
+return True
 
-    def emptyline(self):
-        """Do nothing on empty line."""
-        pass
+def emptyline(self):
+"""Do nothing on empty line."""
+pass
 
-    def do_EOF(self, line):
-        """Exit on EOF."""
-        print("")
-        return True
+def do_EOF(self, line):
+"""Exit on EOF."""
+print("")
+return True
 
 if __name__ == "__main__":
-    console = Console()
-    console.cmdloop()
+console = Console()
+console.cmdloop()
