a020c744f66542e5d5f76530d4df4dad2271e5ed
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-28 15:37:29 -0800

    .

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,58 +1,36 @@
+#!/usr/bin/python3
 import unittest
-from models.engine.file_storage import FileStorage
+import os
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
 
 class TestFileStorage(unittest.TestCase):
+    """Test cases for FileStorage class."""
+
     def setUp(self):
-        """Set up test environment."""
+        """Set up method to prepare test environment."""
         self.storage = FileStorage()
 
-    def test_all(self):
-        """Test retrieval of all objects."""
-        # Add some objects to storage
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-        self.storage.new(obj1)
-        self.storage.new(obj2)
-        self.storage.save()
-
-        # Test if all objects are retrieved
-        all_objs = self.storage.all()
-        self.assertIsInstance(all_objs, dict)
-        self.assertIn(obj1.__class__.__name__ + '.' + obj1.id, all_objs)
-        self.assertIn(obj2.__class__.__name__ + '.' + obj2.id, all_objs)
-
-    def test_new(self):
-        """Test addition of a new object."""
-        obj = BaseModel()
-        self.storage.new(obj)
-        all_objs = self.storage.all()
-        self.assertIn(obj.__class__.__name__ + '.' + obj.id, all_objs)
-
-    def test_save(self):
-        """Test saving objects to file."""
-        obj = BaseModel()
-        self.storage.new(obj)
-        self.storage.save()
-
-        # Check if the file has been created
-        with open(self.storage._FileStorage__file_path, 'r') as file:
-            content = file.read()
-            self.assertIn(obj.__class__.__name__ + '.' + obj.id, content)
+    def tearDown(self):
+        """Clean up method to reset test environment."""
+        if os.path.exists(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
     def test_reload(self):
         """Test reloading objects from file."""
+        # Save some data
         obj = BaseModel()
-        self.storage.new(obj)
-        self.storage.save()
-        
-        # Clear storage
-        del self.storage._FileStorage__objects
-
-        # Reload and check if the object exists
+        obj.save()
+        # Get the current state of __objects
+        current_objects = dict(self.storage._FileStorage__objects)
+        # Reload the data
         self.storage.reload()
-        all_objs = self.storage.all()
-        self.assertIn(obj.__class__.__name__ + '.' + obj.id, all_objs)
+        # Check if the objects are reloaded correctly
+        for key, value in current_objects.items():
+            self.assertIn(key, self.storage._FileStorage__objects)
+            self.assertEqual(value.__class__.__name__, self.storage._FileStorage__objects[key].__class__.__name__)
+
 
 if __name__ == '__main__':
     unittest.main()
