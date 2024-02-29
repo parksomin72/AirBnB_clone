d39854df26b003bb07a420fec828a15d3aac19e3
parksomin72 <fatimazahraealawi@gmail.com>
2024-02-29 06:01:08 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,50 +1,24 @@
-#!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
-import json
-
+from models.base_model import BaseModel
+from models.user import User
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
+    """Class to serialize and deserialize instances to JSON file and vice versa."""
+    
+    ...
+    
+    __classes = {
+        'BaseModel': BaseModel,
+        'User': User
+    }
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
-
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
+    ...
+    
+    def deserialize(self):
+        """Deserializes the JSON file to create instances."""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                data = json.load(f)
+                for k, v in data.items():
+                    cls_name = v['__class__']
+                    if cls_name in self.__classes:
+                        self.__objects[k] = self.__classes[cls_name](**v)
--- models/user.py
+++ models/user.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """User class that inherits from BaseModel."""
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes User instance."""
+        super().__init__(*args, **kwargs)
+
